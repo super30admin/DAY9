a73d9b6fcf3e64144ca57dd3ca8eabebcee67f72
Smit Uplenchwar <smituplenchwar@smits-mbp.mynetworksettings.com>
2024-11-06 09:58:01 -0500

    Done DP-3

--- DeleteAndEarn.py
+++ DeleteAndEarn.py
@@ -1,33 +1,21 @@
-def points1(nums, x, points):
-    if not nums:
-        return nums, points
-    
-    points = points + nums[x]
-    i = 0
-    nums1 = nums.copy()
-    for i in range(len(nums)):
-        if nums[i] == nums[x] or nums[i] == nums[x]-1 or nums[i] == nums[x]+1:
-            nums1.remove(nums[i])
-    return nums1, points
-
-
 def deleteAndEarn(nums) -> int:
-    maxpoints = 0
+    max1 = 0
+    for i in nums:
+        if i > max1:
+            max1 = i
+    arr = [0] * (max1+1)
     for i in range(len(nums)):
-        nums1 = nums.copy()
-        points = 0
+        arr[nums[i]] += 1
+
+    for i in range(len(arr)):
+        arr[i] = arr[i] * i
+
+    if len(nums) >= 2:
+        for i in range(2,len(arr)):
+            arr[i] = max(arr[i-1], arr[i-2]+arr[i])
         
-        while nums1:
-            i = i%len(nums1)
-            nums1, points = points1(nums1, i, points)
-            
-        maxpoints = max(points, maxpoints)
-            
-    return maxpoints
+    return max(arr[-2],arr[-1])
 
-
-nums = [2,2,3,3,3,4]
+nums = [3,4,2]
 
 print(deleteAndEarn(nums))
-
-    
