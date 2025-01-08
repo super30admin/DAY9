55e5eb5ec12f6c5b77976e98b4928b98fd4c75f8
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-08 12:11:57 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,5 +3,36 @@
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
+// Time complexity:O(n^2)
+// Space complexity:O(1)
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int result=0;
+
+        for (int i=n-2;i>=0;i--) {
+            for (int j=0;j<matrix[i].length;j++) {
+                if(j==0) {
+                    matrix[i][j] = matrix[i][j] + Math.min(matrix[i+1][j] , matrix[i+1][j+1]);
+                }
+                else if (0<j && j<matrix[i].length-1) {
+                    matrix[i][j] = matrix[i][j] + Math.min(matrix[i+1][j-1], Math.min(matrix[i+1][j] , matrix[i+1][j+1]));
+                } 
+                else if(j==matrix[i].length-1) {
+                    matrix[i][j] = matrix[i][j] + Math.min(matrix[i+1][j-1] , matrix[i+1][j]);
+                }
+            }
+        }
+
+
+        for (int i=matrix[0].length-2;i>=0;i--){
+            matrix[0][i] = Math.min(matrix[0][i], matrix[0][i+1]);
+        }
+        
+
+        return matrix[0][0];
+        
+    }
+}
