c3a5a4881435c728eda466a60800fba465fecbe1
Shiv Dalmia <shivam.dalmia@gmail.com>
2025-01-08 19:22:23 -0800

    DeleteAndEarn, MinFallingPathSum (DP-3)

--- DeleteAndEarn.java
+++ DeleteAndEarn.java
@@ -2,13 +2,16 @@
 // Space Complexity : O ( maxElem )
 // Did this code successfully run on Leetcode : Yes
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
- * By storing each element upto the max along with their respective values
+ * By storing each element up to the max along with their respective values
  * we can convert this to a house-robber pattern
  * We can use a map to store the pairs, or an array - an array would
- * be faster and simpler to code, but would need upto maxElem space
+ * be faster and simpler to code, but would need up to maxElem space
  * A map would be slightly slower and need a condition for when the element
- * is not present but would me more space efficient, especially if the
+ * is not present but would be more space efficient, especially if the
  * elements are sparsely distributed.
  */
 class DeleteAndEarn {
@@ -22,14 +25,14 @@ class DeleteAndEarn {
         for(int num: nums) {
             if (max < num) max = num;
             freqs.putIfAbsent(num, 0);
-            freqs.compute(num, (k, v) -> v + num);
+            freqs.compute(num, (_, v) -> v + num);
         }
 
         // do bottom up dp
         int skip = 0;
         int take = 0;
 
-        // for each possible element upto max
+        // for each possible element up to max
         for(int i = 1; i <= max; i++) {
             // if the element is present
             if(freqs.containsKey(i)) {
